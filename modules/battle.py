c99430a388adbf0f3234608f4c2d7d87ca6648b3
BoiarkinaOryna <boyarkina.ar@gmail.com>
2023-08-15 14:38:22 +0300

    step-by-step function fixing

--- modules/battle.py
+++ modules/battle.py
@@ -20,41 +20,44 @@ def strike(x_click, y_click, window, start, list_coordinates_x, list_cells, next
                 if x_click >= list_coordinates_x[index_x] and x_click < list_coordinates_x[index_x + 1]:
                     for count_y in m_data.list_coordinates_y:
                         index_y = index_y + 1
-                        m_coordinates.search_index(cor_x = count_x, cor_y = count_y)
-                        if list_cells == m_data.bot_list_cells:
-                            print("m_data.index =", m_data.index)
-                            print("list_cells[m_data.index] =", list_cells[m_data.index])
-                            print("list_cells[m_data.index] // 10 =", list_cells[m_data.index] // 10)
-                        if list_cells[m_data.index] // 10 != 1:
-                            for n in range(1):
-                                # print("A")
-                                if index_y < 10:
-                                    if list_cells == m_data.bot_list_cells:
-                                        print("y_click =", y_click)
-                                        print("m_data.list_coordinates_y[index_y] =", m_data.list_coordinates_y[index_y])
-                                        print("m_data.list_coordinates_y[index_y + 1] =", m_data.list_coordinates_y[index_y + 1])
-                                    if y_click >= m_data.list_coordinates_y[index_y] and y_click < m_data.list_coordinates_y[index_y + 1]:
-                                        if list_cells == m_data.bot_list_cells:
-                                            print("2m_data.index =", m_data.index)
-                                        if list_cells[m_data.index] == 0 or list_cells[m_data.index] == 5 or list_cells[m_data.index] == 10:
-                                            cross = m_settings.Cross(x1 = count_x + 5, y1 = count_y + 5)
-                                            cross.blit_sprite(screen_game = window)
-                                            next_step1 = True
-                                            print("cross")
-                                            list_cells[m_data.index] = 10
+                        if index_y < 10:
+                            if y_click >= m_data.list_coordinates_y[index_y] and y_click < m_data.list_coordinates_y[index_y + 1]:
+                                m_coordinates.search_index(cor_x = count_x, cor_y = count_y)
+                                if list_cells == m_data.bot_list_cells:
+                                    print("count_y =", count_y)
+                                    print("m_data.index =", m_data.index)
+                                    print("list_cells[m_data.index] =", list_cells[m_data.index])
+                                    print("list_cells[m_data.index] // 10 =", list_cells[m_data.index] // 10)
+                                if list_cells[m_data.index] // 10 != 1:
+                                    for n in range(1):
+                                        # print("A")
+                                        if index_y < 10:
+                                            if list_cells == m_data.bot_list_cells:
+                                                print("y_click =", y_click)
+                                                print("m_data.list_coordinates_y[index_y] =", m_data.list_coordinates_y[index_y])
+                                                print("m_data.list_coordinates_y[index_y + 1] =", m_data.list_coordinates_y[index_y + 1])
+                                            if y_click >= m_data.list_coordinates_y[index_y] and y_click < m_data.list_coordinates_y[index_y + 1]:
+                                                if list_cells == m_data.bot_list_cells:
+                                                    print("2m_data.index =", m_data.index)
+                                                if list_cells[m_data.index] == 0 or list_cells[m_data.index] == 5 or list_cells[m_data.index] == 10:
+                                                    cross = m_settings.Cross(x1 = count_x + 5, y1 = count_y + 5)
+                                                    cross.blit_sprite(screen_game = window)
+                                                    next_step1 = True
+                                                    print("cross")
+                                                    list_cells[m_data.index] = 10
+                                                    break
+                                                else:
+                                                    explosion = m_settings.Explosion(x1 = count_x + 5, y1 = count_y + 5)
+                                                    explosion.blit_sprite(screen_game = window)
+                                                    next_step1 = True
+                                                    print("explosion")
+                                                    m_destruction.defated_ship(list_cells1 = list_cells, index1 = m_data.index, x1 = count_x, y1 = count_y, screen1 = window)
+                                                    break
                                             break
-                                        else:
-                                            explosion = m_settings.Explosion(x1 = count_x + 5, y1 = count_y + 5)
-                                            explosion.blit_sprite(screen_game = window)
-                                            next_step1 = True
-                                            print("explosion")
-                                            m_destruction.defated_ship(list_cells1 = list_cells, index1 = m_data.index, x1 = count_x, y1 = count_y, screen1 = window)
-                                            break
-                                    break
-                        else:
-                            print("//////////////////")
-                            next_step1 = False
-                        return next_step1
+                                else:
+                                    print("//////////////////")
+                                    next_step1 = False
+                                return next_step1
 def control_step(screen, x, y):
     print("m_data.bot_step =", m_data.bot_step)
     if m_data.bot_step == False:
